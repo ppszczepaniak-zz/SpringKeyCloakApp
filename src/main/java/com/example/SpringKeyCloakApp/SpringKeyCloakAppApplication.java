37b81c35b9cdd0c385acc3cc54a775a2b4d637a6
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-11-30 12:29:05 +0100

    initial commit

---
+++ src/main/java/com/example/SpringKeyCloakApp/SpringKeyCloakAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringKeyCloakApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeyCloakAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKeyCloakAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringKeyCloakApp/SpringKeyCloakAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringKeyCloakApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeyCloakAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
