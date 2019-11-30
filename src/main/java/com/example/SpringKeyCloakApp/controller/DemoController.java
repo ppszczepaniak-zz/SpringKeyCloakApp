8babcd9c95239f1329d3d43fd4f9811d5d72be9b
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-11-30 13:25:36 +0100

    add basic controller outputs

--- src/main/java/com/example/SpringKeyCloakApp/controller/DemoController.java
+++ src/main/java/com/example/SpringKeyCloakApp/controller/DemoController.java
@@ -1,6 +1,5 @@
 package com.example.SpringKeyCloakApp.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +8,25 @@ public class DemoController {
 
     @GetMapping(path = "/")
     public String index() {
-        return "This is freely accessible page";
+        return "<center>" +
+                "<font size=\"5\">" +
+                "<p>Hello stranger, this is a freely accessible page.</p>\n" +
+                "<p>In order to visit page for registered users, please click " +
+                "<a href=\"http://127.0.0.1:8081/user\">here</a>.</p>" +
+                "</font>" +
+                "</center>";
+
     }
+
+    //TODO consider https here
 
     @GetMapping(value = "/user")
     public String user() {
-        return "This page is for users only";
+        return "<center>" +
+                "<font size=\"5\">" +
+                "<p>Welcome user.</p>\n" +
+                "<p>This page is for registered users only!</p>" +
+                "</font>" +
+                "</center>";
     }
 }
